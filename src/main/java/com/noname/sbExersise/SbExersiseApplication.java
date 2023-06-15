ec308aae25db0f97851c1b50fe322f0513d3c3e5
makemeha2 <makemeha2@gmail.com>
2023-06-16 07:57:52 +0900

    최초 프로젝트

---
+++ src/main/java/com/noname/sbExersise/SbExersiseApplication.java
@@ -0,0 +1,13 @@
+package com.noname.sbExersise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbExersiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbExersiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noname/sbExersise/SbExersiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noname.sbExersise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbExersiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
